ce0556071ee5d7a9a6550ae7281c9474f7bac8f2
Olya Markysh <december.olka51@gmail.com>
2020-09-01 19:32:17 +0300

    Updates

---
+++ precache-manifest.71b2394ab2fdd4d70d8dbf24f8bd7b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e7a24f5e04b7ca3864f1c9877cca0a",
+    "url": "/test-task-abz/index.html"
+  },
+  {
+    "revision": "bc478a07e23ae5164814",
+    "url": "/test-task-abz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d08afaf623ac475432a",
+    "url": "/test-task-abz/static/js/2.97945c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task-abz/static/js/2.97945c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc478a07e23ae5164814",
+    "url": "/test-task-abz/static/js/main.982d20ec.chunk.js"
+  },
+  {
+    "revision": "b3cc179dab9777935465",
+    "url": "/test-task-abz/static/js/runtime-main.fd50fa42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-task-abz/static/media/logo.5d5d9eef.svg"
+  }
+]);
